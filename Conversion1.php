d0eb8247e8deeb489a1cebb425ad61113b174c96
DelfinKat <kat.delfin13@gmail.com>
2016-03-21 01:17:07 +0800

    New file: Conversion1.php

---
+++ Conversion1.php
@@ -0,0 +1,16 @@
+<?php 
+	header ("url=Conversion.php"); 
+	include("dbconnection.php"); 
+
+	$sql_query = "INSERT INTO conversion(unit_id1, qty1, unit_id2, qty2)
+			VALUES ('" . $_POST['unitM1'] . "," . $_POST['qty1'] . "," . $_POST['unitM2'] . "," . $_POST['qty2'] . ")"; 
+
+	mysqli_query($connect, $sql_query); 
+		
+	echo "Conversion added!";
+
+?>
+
+<html>
+<a href="Conversion.php">Go Back</a>
+</html>
